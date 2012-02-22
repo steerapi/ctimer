f4d5480a4818216dfba4ab190c1efb82d157a5e0
Surat Teerapittayanon <steerapi@gmail.com>
2012-02-22 14:44:51 -0500

    dynamically sized thread pool

--- timer/timer.h
+++ timer/timer.h
@@ -24,6 +24,7 @@ typedef struct TimeItem {
 	struct TimeItem* next;
 	struct nanotime interval;
 	action callback;
+	action cleanup;
 	void *args;
 	pthread_mutex_t lock;
 	pthread_cond_t conv;
@@ -34,6 +35,43 @@ typedef struct TimeItem {
 
 #define NANO 1000000000
 
+//Watch out for race condition when use in multiple threads
+TimeItem free_pipe;
+int free_count;
+int total_count;
+
+void cleanupInterval(TimeItem *ti) {
+	ti->canceled = 1;
+
+	if (ti->cleanup)
+		ti->cleanup(ti->args);
+	if (free_count > 2 * total_count / 3) {
+		pipe_producer_free(ti->in);
+		pipe_consumer_free(ti->out);
+		total_count--;
+	} else {
+		TimeItem *next = free_pipe.next;
+		free_pipe.next = ti;
+		ti->next = next;
+		free_count++;
+	}
+//	printf("%d %d\n", free_count, total_count);
+//	pipe_producer_free(ti->pi->in);
+//	pipe_consumer_free(ti->pi->out);
+}
+
+void clearInterval(TimeItem *ti) {
+	if (ti == NULL || ti->canceled == 1)
+		return;
+	//TODO: Watch for race condition
+	pthread_mutex_lock(&ti->lock);
+	pthread_cond_signal(&ti->conv);
+	pthread_cond_wait(&ti->canceledv, &ti->lock);
+	pthread_mutex_unlock(&ti->lock);
+
+	cleanupInterval(ti);
+}
+
 static void handle_interval(const void* elem_in, size_t count,
 		pipe_producer_t* elem_out, void* aux) {
 	TimeItem *ti = ((TimeItem*) aux);
@@ -42,6 +80,7 @@ static void handle_interval(const void* elem_in, size_t count,
 		free(ti);
 		return;
 	}
+
 	//TODO: Watch for race condition
 	pthread_mutex_lock(&ti->lock);
 	if (ti->canceled) {
@@ -62,13 +101,13 @@ static void handle_interval(const void* elem_in, size_t count,
 		if (ti->times > 0) {
 			ti->times--;
 			if (ti->times == 0) {
-				//TODO: Watch for race condition
-				clearInterval(ti);
+				cleanupInterval(ti);
 			}
 		}
 		//update deadline
 		deadline = nanotime_now().ns + ti->interval.ns;
 	}
+
 	//queue up for next round
 	pipe_push(ti->in, &deadline, 1);
 	timeleft = deadline - nanotime_now().ns;
@@ -83,51 +122,25 @@ static void handle_interval(const void* elem_in, size_t count,
 		pthread_mutex_unlock(&ti->lock);
 //		nanosleep(&ts, 0);
 	}
+
 	return;
 }
 
-//Watch out for race condition when use in multiple threads
-TimeItem free_pipe;
-int free_count;
-int total_count;
-
-void clear_pool() {
+void clear_pool(int num) {
 	TimeItem * torem = free_pipe.next;
-	while (torem != NULL) {
+	int count = 0;
+	while (torem != NULL && (num == 0 || count < num)) {
 		free_pipe.next = free_pipe.next->next;
 		free_count--;
 		pipe_producer_free(torem->in);
 		pipe_consumer_free(torem->out);
 		torem = free_pipe.next;
+		count++;
 	}
 }
 
-void clearInterval(TimeItem *ti) {
-	if (ti == NULL)
-		return;
-	//TODO: Watch for race condition
-	pthread_mutex_lock(&ti->lock);
-	ti->canceled = 1;
-	pthread_cond_signal(&ti->conv);
-	pthread_cond_wait(&ti->canceledv, &ti->lock);
-	pthread_mutex_unlock(&ti->lock);
-
-//	if (free_count > 2*total_count/3) {
-//		pipe_producer_free(ti->in);
-//		pipe_consumer_free(ti->out);
-//		total_count--;
-//	} else {
-	TimeItem *next = free_pipe.next;
-	free_pipe.next = ti;
-	ti->next = next;
-	free_count++;
-//	}
-//	printf("%d %d\n", free_count, total_count);
-//	pipe_producer_free(ti->pi->in);
-//	pipe_consumer_free(ti->pi->out);
-}
-
-TimeItem *setInterval(uint64_t nsec, action callback, void* args, int times) {
+TimeItem *setInterval(uint64_t nsec, action callback, action cleanup,
+		void* args, int times) {
 	TimeItem *ti;
 	if (free_pipe.next == NULL) {
 		ti = malloc(sizeof(TimeItem));
@@ -150,6 +163,7 @@ TimeItem *setInterval(uint64_t nsec, action callback, void* args, int times) {
 		free_pipe.next = free_pipe.next->next;
 		free_count--;
 	}
+	ti->cleanup = cleanup;
 	ti->callback = callback;
 	ti->args = args;
 	ti->times = times;
@@ -161,8 +175,8 @@ TimeItem *setInterval(uint64_t nsec, action callback, void* args, int times) {
 	return ti;
 }
 
-TimeItem *setTimeout(uint64_t nsec, action callback, void* args) {
-	return setInterval(nsec, callback, args, 1);
+TimeItem *setTimeout(uint64_t nsec, action callback, action cleanup, void* args) {
+	return setInterval(nsec, callback, cleanup, args, 1);
 }
 
 #endif /* TIMER_H_ */
